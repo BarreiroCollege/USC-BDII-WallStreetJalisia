2ee173f49067ad9bcd74727f7a6e8c58e98dc863
Diego Barreiro <diego.barreiro.perez@gmail.com>
2021-03-27 18:52:15 +0100

    Cambios

--- src/gal/sdc/usc/wallstreet/model/Usuario.java
+++ src/gal/sdc/usc/wallstreet/model/Usuario.java
@@ -1,4 +1,170 @@
 package gal.sdc.usc.wallstreet.model;
 
+import java.util.Objects;
+
 public class Usuario {
+    private String identificador;
+    private String clave;
+    private String direccion;
+    private String cp;
+    private String localidad;
+    private Integer telefono;
+    private Float saldo;
+    private Float saldoBloqueado = 0.f;
+    private Boolean activo;
+
+    private Usuario() {
+    }
+
+    public String getIdentificador() {
+        return identificador;
+    }
+
+    public void setIdentificador(String identificador) {
+        this.identificador = identificador;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCp() {
+        return cp;
+    }
+
+    public void setCp(String cp) {
+        this.cp = cp;
+    }
+
+    public String getLocalidad() {
+        return localidad;
+    }
+
+    public void setLocalidad(String localidad) {
+        this.localidad = localidad;
+    }
+
+    public Integer getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(Integer telefono) {
+        this.telefono = telefono;
+    }
+
+    public Float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Float saldo) {
+        this.saldo = saldo;
+    }
+
+    public Float getSaldoBloqueado() {
+        return saldoBloqueado;
+    }
+
+    public void setSaldoBloqueado(Float saldoBloqueado) {
+        this.saldoBloqueado = saldoBloqueado;
+    }
+
+    public Boolean getActivo() {
+        return activo;
+    }
+
+    public void setActivo(Boolean activo) {
+        this.activo = activo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return identificador.equals(usuario.identificador);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(identificador);
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "identificador='" + identificador + '\'' +
+                ", clave='" + clave + '\'' +
+                ", direccion='" + direccion + '\'' +
+                ", cp='" + cp + '\'' +
+                ", localidad='" + localidad + '\'' +
+                ", telefono=" + telefono +
+                ", saldo=" + saldo +
+                ", saldoBloqueado=" + saldoBloqueado +
+                ", activo=" + activo +
+                '}';
+    }
+
+    public static class Builder {
+        private final Usuario usuario = new Usuario();
+
+        public Builder(String identificador) {
+            usuario.identificador = identificador;
+        }
+
+        public Builder withClave(String clave) {
+            usuario.clave = clave;
+            return this;
+        }
+
+        public Builder withDireccion(String direccion) {
+            usuario.direccion = direccion;
+            return this;
+        }
+
+        public Builder withCp(String cp) {
+            usuario.cp = cp;
+            return this;
+        }
+
+        public Builder withLocalidad(String localidad) {
+            usuario.localidad = localidad;
+            return this;
+        }
+
+        public Builder withTelefono(Integer telefono) {
+            usuario.telefono = telefono;
+            return this;
+        }
+
+        public Builder withSaldo(Float saldo) {
+            usuario.saldo = saldo;
+            return this;
+        }
+
+        public Builder withSaldoBloqueado(Float saldoBloqueado) {
+            usuario.saldoBloqueado = saldoBloqueado;
+            return this;
+        }
+
+        public Builder withActivo(Boolean activo) {
+            usuario.activo = activo;
+            return this;
+        }
+
+        public Usuario build() {
+            return usuario;
+        }
+    }
 }
--- src/gal/sdc/usc/wallstreet/repository/UsuarioDAO.java
+++ src/gal/sdc/usc/wallstreet/repository/UsuarioDAO.java
@@ -1,12 +1,32 @@
 package gal.sdc.usc.wallstreet.repository;
 
+import gal.sdc.usc.wallstreet.model.Usuario;
 import gal.sdc.usc.wallstreet.util.DAO;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class UsuarioDAO extends DAO {
     public UsuarioDAO(Connection conexion) {
         super(conexion);
         System.out.println("UsuarioDAO instanciado");
+    }
+
+    public Usuario getUsuarioPorIdentificador(String identificador) {
+        Usuario usuario = null;
+
+        try (PreparedStatement ps = super.conexion.prepareStatement("SELECT * FROM usuario WHERE identificador=?")) {
+            ps.setString(1, identificador);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                usuario = Usuario.Builder(rs.getString("identificador"))
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return usuario;
     }
 }
