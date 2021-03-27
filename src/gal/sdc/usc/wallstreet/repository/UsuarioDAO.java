ab24a06f30e9208d59e18cab7a1f9c3ff33cf2de
Diego Barreiro <diego.barreiro.perez@gmail.com>
2021-03-27 18:53:05 +0100

    Cambios 2

--- src/gal/sdc/usc/wallstreet/repository/UsuarioDAO.java
+++ src/gal/sdc/usc/wallstreet/repository/UsuarioDAO.java
@@ -8,5 +8,6 @@ public class UsuarioDAO extends DAO {
     public UsuarioDAO(Connection conexion) {
         super(conexion);
         System.out.println("UsuarioDAO instanciado");
+        // jola
     }
 }
