4aaac99acdadfe5f75bc8eab73f6db70275003d4
Diego Barreiro <diego.barreiro.perez@gmail.com>
2021-03-31 14:06:46 +0200

    Arreglos y optimizaciones

--- src/gal/sdc/usc/wallstreet/model/ddl/Entidad.java
+++ src/gal/sdc/usc/wallstreet/model/ddl/Entidad.java
@@ -1,8 +1,8 @@
 package gal.sdc.usc.wallstreet.model.ddl;
 
+import gal.sdc.usc.wallstreet.util.LectorDinamico;
+
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 
 /**
  * Clase abstracta estándar de una entidad
@@ -16,12 +16,9 @@ public abstract class Entidad {
 
             if (field.getAnnotation(Columna.class).pk()) {
                 if (Entidad.class.isAssignableFrom(field.getType())) {
-                    try {
-                        Method method = this.getClass().getMethod("get" + nombre.substring(0, 1).toUpperCase() + nombre.substring(1));
-                        Entidad valor = (Entidad) method.invoke(this);
+                    Entidad valor = LectorDinamico.llamarGetter(nombre, this);
+                    if (valor != null) {
                         pks += valor.getNumPks();
-                    } catch (IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
-                        e.printStackTrace();
                     }
                 } else {
                     pks++;
