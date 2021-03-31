91704e9b4516906576fac3d4b657b1a2cb5000d6
Diego Barreiro <diego.barreiro.perez@gmail.com>
2021-03-31 14:07:01 +0200

    Más ejemplos

--- src/gal/sdc/usc/wallstreet/controller/SampleController.java
+++ src/gal/sdc/usc/wallstreet/controller/SampleController.java
@@ -1,10 +1,16 @@
 package gal.sdc.usc.wallstreet.controller;
 
 import com.jfoenix.controls.JFXButton;
+import gal.sdc.usc.wallstreet.model.Empresa;
 import gal.sdc.usc.wallstreet.model.Pago;
 import gal.sdc.usc.wallstreet.model.PagoUsuario;
+import gal.sdc.usc.wallstreet.model.Participacion;
+import gal.sdc.usc.wallstreet.model.Usuario;
+import gal.sdc.usc.wallstreet.repository.EmpresaDAO;
 import gal.sdc.usc.wallstreet.repository.PagoDAO;
 import gal.sdc.usc.wallstreet.repository.PagoUsuarioDAO;
+import gal.sdc.usc.wallstreet.repository.ParticipacionDAO;
+import gal.sdc.usc.wallstreet.repository.UsuarioDAO;
 import gal.sdc.usc.wallstreet.repository.helpers.DatabaseLinker;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -17,21 +23,51 @@ public class SampleController extends DatabaseLinker {
     private JFXButton prueba;
 
     public void prueba(ActionEvent actionEvent) {
-        List<PagoUsuario> pagos = super
+        /* List<PagoUsuario> pagos = super
                 .getDAO(PagoUsuarioDAO.class)
                 .getPagosHastaAhora();
         for (PagoUsuario pago : pagos) {
             System.out.println(pago);
-        }
+        } */
 
-        Pago pago = new Pago.Builder(new Date(), pagos.get(0).getPago().getEmpresa())
+        /* Pago pago = new Pago.Builder(new Date(), pagos.get(0).getPago().getEmpresa())
                 .withBeneficioPorParticipacion(3.1f)
                 .build();
         PagoUsuario pagoUsuario = new PagoUsuario.Builder(pagos.get(0).getUsuario(), pago)
                 .withNumParticipaciones(30)
                 .build();
+
         super.getDAO(PagoUsuarioDAO.class).crear(pagoUsuario);
         super.getDAO(PagoUsuarioDAO.class).borrar(pagoUsuario);
-        super.getDAO(PagoDAO.class).borrar(pago);
+        super.getDAO(PagoDAO.class).borrar(pago); */
+
+        /* Empresa empresa = new Empresa.Builder()
+                .withUsuario(
+                        new Usuario.Builder()
+                        .withIdentificador("mudi")
+                        .withClave("Mudi")
+                        .withSaldo(0f)
+                        .build()
+                )
+                .withCif("Pepito")
+                .withNombre("Palotes")
+                .build();
+
+        super.getDAO(EmpresaDAO.class).crear(empresa); */
+
+        /* Usuario diego = super.getDAO(UsuarioDAO.class).seleccionar("diego");
+        Empresa diegoEmpresa = super.getDAO(EmpresaDAO.class).seleccionar(diego);
+
+        Participacion participacion = new Participacion.Builder()
+                .withEmpresa(diegoEmpresa)
+                .withUsuario(diego)
+                .withCantidad(65)
+                .build();
+
+        super.getDAO(ParticipacionDAO.class).crear(participacion); */
+
+        /* for (Usuario usuario : super.getDAO(UsuarioDAO.class).getUsuarios()) {
+            System.out.println(usuario);
+        } */
     }
 }
--- src/gal/sdc/usc/wallstreet/repository/UsuarioDAO.java
+++ src/gal/sdc/usc/wallstreet/repository/UsuarioDAO.java
@@ -2,11 +2,32 @@ package gal.sdc.usc.wallstreet.repository;
 
 import gal.sdc.usc.wallstreet.model.Usuario;
 import gal.sdc.usc.wallstreet.repository.helpers.DAO;
+import gal.sdc.usc.wallstreet.util.Mapeador;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UsuarioDAO extends DAO<Usuario> {
     public UsuarioDAO(Connection conexion) {
         super(conexion, Usuario.class);
+    }
+
+    public List<Usuario> getUsuarios() {
+        List<Usuario> usuarios = new ArrayList<>();
+        try (PreparedStatement ps = super.conexion.prepareStatement(
+                "SELECT * FROM usuario"
+        )) {
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                usuarios.add(Mapeador.map(rs, Usuario.class));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return usuarios;
     }
 }
