b78730597548dbcb2881658420c93a0746b680cb
Nerea <nereafreiria06@rai.usc.es>
2021-04-12 17:22:05 +0200

    Cabeceras principales

--- src/gal/sdc/usc/wallstreet/controller/VCompraController.java
+++ src/gal/sdc/usc/wallstreet/controller/VCompraController.java
@@ -1,6 +1,28 @@
 package gal.sdc.usc.wallstreet.controller;
 
+import com.jfoenix.controls.JFXButton;
 import gal.sdc.usc.wallstreet.repository.helpers.DatabaseLinker;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
 
 public class VCompraController extends DatabaseLinker {
+    @FXML
+    private JFXButton btnSalir;
+
+
+    public void btnSalirEvent(ActionEvent event) {
+        Stage stage = (Stage) btnSalir.getScene().getWindow();
+        stage.close();
+    }
+
+    public void buscarPorNombre(ActionEvent event){
+
+    }
+
+    public void buscarPorPrecio(ActionEvent event){
+
+    }
+
+
 }
