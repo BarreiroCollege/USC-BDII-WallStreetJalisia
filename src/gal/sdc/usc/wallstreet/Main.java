d2545ea83e5ea6c8a498a3db478c93af87d41ea7
mo-ashour <72417294+mo-ashour@users.noreply.github.com>
2021-04-13 23:19:44 +0200

    Cambios

--- src/gal/sdc/usc/wallstreet/Main.java
+++ src/gal/sdc/usc/wallstreet/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("view/principal_inversor.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("view/principal_empresa.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 683, 551));
         primaryStage.show();
--- src/gal/sdc/usc/wallstreet/controller/PrincipalEmpresaController.java
+++ src/gal/sdc/usc/wallstreet/controller/PrincipalEmpresaController.java
@@ -1,6 +1,7 @@
 package gal.sdc.usc.wallstreet.controller;
 
 import com.jfoenix.controls.JFXButton;
+import gal.sdc.usc.wallstreet.Main;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -9,25 +10,33 @@ import javafx.scene.Scene;
 import javafx.scene.shape.Circle;
 
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class PrincipalEmpresaController {
     @FXML
-    private Circle buttonPerfilUsuario;
+    private JFXButton buttonPerfilUsuario;
     @FXML
     private JFXButton buttonParticipaciones;
+    @FXML
+    private JFXButton verPerfilButton;
     Parent principalEmpresa;
     Scene scene;
-    public PrincipalEmpresaController() throws IOException {
-        principalEmpresa = FXMLLoader.load(getClass().getResource("view/principal_empresa.fxml"));
-        scene = new Scene(principalEmpresa);
+
+    public void Initialize(){
+        buttonPerfilUsuario.setOnAction(event -> {
+            Parent root;
+            try{
+                root = FXMLLoader.load(Main.class.getResource("view/principal_empresa.fxml"));
+                Stage stage = new Stage();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        verPerfilButton.setOnAction(event -> {
+
+        });
     }
-
-    EventHandler<MouseEvent> clickPerfil = new EventHandler<MouseEvent>(){
-        @Override
-        public void handle(MouseEvent e) {
-
-        }
-    };
 }
