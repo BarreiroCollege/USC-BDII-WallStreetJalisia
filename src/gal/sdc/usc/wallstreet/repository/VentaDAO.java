ee9cfd030dddf7771a907c908b41f5a44776145f
Guille <guillermo.blanco.filgueira@rai.usc.es>
2021-04-19 09:47:46 +0200

    temp5

---
+++ src/gal/sdc/usc/wallstreet/repository/VentaDAO.java
@@ -0,0 +1,11 @@
+package gal.sdc.usc.wallstreet.repository;
+
+import gal.sdc.usc.wallstreet.model.Venta;
+import gal.sdc.usc.wallstreet.repository.helpers.DAO;
+import java.sql.Connection;
+
+public class VentaDAO extends DAO<Venta> {
+    public VentaDAO(Connection conexion) {
+        super(conexion, Venta.class);
+    }
+}
