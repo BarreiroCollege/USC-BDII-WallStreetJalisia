34b23f79621d5c163adc80ac2ccfe7a28d23cb1b
Diego Barreiro <diego.barreiro.perez@gmail.com>
2021-04-21 20:00:54 +0200

    Corrección de Tabla

--- src/gal/sdc/usc/wallstreet/model/SuperUsuario.java
+++ src/gal/sdc/usc/wallstreet/model/SuperUsuario.java
@@ -2,9 +2,11 @@ package gal.sdc.usc.wallstreet.model;
 
 import gal.sdc.usc.wallstreet.model.ddl.Columna;
 import gal.sdc.usc.wallstreet.model.ddl.Entidad;
+import gal.sdc.usc.wallstreet.model.ddl.Tabla;
 
 import java.util.Objects;
 
+@Tabla("superusuario")
 public class SuperUsuario extends Entidad {
     @Columna(value = "identificador", pk = true)
     private String identificador;
