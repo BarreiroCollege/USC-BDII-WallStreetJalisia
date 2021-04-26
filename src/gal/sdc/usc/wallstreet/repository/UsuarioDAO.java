6c91640028231a235f48546fb64de41133408773
Diego Barreiro <diego.barreiro.perez@gmail.com>
2021-04-26 11:08:32 +0200

    Eliminar método

--- src/gal/sdc/usc/wallstreet/repository/UsuarioDAO.java
+++ src/gal/sdc/usc/wallstreet/repository/UsuarioDAO.java
@@ -33,22 +33,6 @@ public class UsuarioDAO extends DAO<Usuario> {
 
     }
 
-    public double getSaldoUsuario(String identificador){
-        double saldo = 0.0;
-        try (PreparedStatement ps = super.conexion.prepareStatement(
-                "SELECT * FROM usuario WHERE identificador = ?"
-        )) {
-            ps.setString(1, identificador);
-            ResultSet rs = ps.executeQuery();
-            if(rs.next()){
-                saldo = rs.getDouble("saldo");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return saldo;
-    }
-
     public Usuario getUsuario(String identificador) {
         return getUsuarios().stream().filter(user -> identificador.equals(user.getSuperUsuario().getIdentificador())).findFirst().orElse(null);
     }
