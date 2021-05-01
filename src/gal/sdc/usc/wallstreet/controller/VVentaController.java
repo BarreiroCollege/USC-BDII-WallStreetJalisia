53672ad559366e87b7778a2807859463b5599fe5
Guille <guillermo.blanco.filgueira@rai.usc.es>
2021-05-01 20:04:45 +0200

    otro merge

--- src/gal/sdc/usc/wallstreet/controller/VVentaController.java
+++ src/gal/sdc/usc/wallstreet/controller/VVentaController.java
@@ -205,7 +205,7 @@ public class VVentaController extends DatabaseLinker {
     }
 
     public void actualizarComision(){
-        campoComision.setText(String.valueOf(super.getDAO(ReguladorDAO.class).getRegulador().getComision()));
+        campoComision.setText(String.valueOf(super.getDAO(ReguladorDAO.class).getRegulador().getComision()*100)+" %");
     }
 
 
